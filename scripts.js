9c5a435e15dc14049b36b3720b5d4a574c7e6006
shaynmothman <172470869+shaynmothman@users.noreply.github.com>
2024-08-12 18:58:08 -0700

    Created basic file structure and added pseudocode comments to scripts.js

---
+++ scripts.js
@@ -0,0 +1,30 @@
+/* Rock Paper Scissors Game */
+
+// Initialize variables for player selections
+// Collect user input (string)
+// Assign user input to Player 1 variable
+// Randomly select Player 2 value
+// Compare values
+// If Player 1 === rock
+    // If Player 2 === paper
+        // Return 'Player 2 wins'
+    // Else if Player 2 === scissors
+        // Return 'Player 1 wins'
+    // Else
+        // Return "It's a tie"
+// Else if Player 1 === paper
+    // If Player 2 === rock
+        // Return 'Player 1 wins'
+    // Else if Player 2 === scissors
+        // Return 'Player 2 wins'
+    // Else 
+        // Return "it's a tie"
+// Else if Player 1 === scissors
+    // If Player 2 === rock
+        // Return 'Player 2 wins'
+    // Else if Player 2 === paper
+        // Return 'Player 1 wins
+    // Else
+        // Return "It's a tie"
+// Else 
+    // Return 'Select rock, paper, or scissors'
